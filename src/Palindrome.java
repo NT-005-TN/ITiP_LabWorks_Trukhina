497a6e70537e9c216a3fc7c17aec1242df12381f
Nastya <trukhina12345@mail.ru>
2025-09-13 21:02:09 +0300

    Добавлены файлы по работе с основами Java - 1 лабораторная

---
+++ src/Palindrome.java
@@ -0,0 +1,22 @@
+public class Palindrome {
+
+    public static boolean isPalindrome(String line){
+        String reversedString = reverseString(line);
+        return line.equals(reversedString);
+    }
+
+    public static String reverseString(String s){
+        String newString = "";
+        for(int i = s.length()-1; i >= 0; i--){
+            newString += s.charAt(i);
+        }
+        return newString;
+    }
+
+    public static void main(String[] args){
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            System.out.println(isPalindrome(s));
+        }
+    }
+}
---
+++ src/Primes.java
@@ -0,0 +1,19 @@
+public class Primes {
+
+    public static boolean isPrime(int n){
+
+        for(int d = 2; d <= Math.ceil(Math.sqrt(n)); d++){
+            if(n % d == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        for(int i = 2; i <= 100; i++){
+            if(isPrime(i) == true){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
